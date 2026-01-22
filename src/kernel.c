9cc00cef90b166882545cf592ae51a8b410bde71
lucasarabi <lucaarab03@gmail.com>
2026-01-22 10:25:52 -0500

    Conducted final review of code before first compilation

--- src/kernel.c
+++ src/kernel.c
@@ -10,26 +10,31 @@ static volatile uint64_t limine_base_revision[] = LIMINE_BASE_REVISION(0);
 
 // Skipping framebuffer request -- we do not need graphics
 
-__attribute__((used, section(".limine_requests")))
+__attribute__((used, section(".limine_requests_start")))
 static volatile uint64_t limine_requests_start_marker[] = LIMINE_REQUESTS_START_MARKER;
 
-__attribute__((used, section(".limine_requests")))
+__attribute__((used, section(".limine_requests_end")))
 static volatile uint64_t limine_requests_end_marker[] = LIMINE_REQUESTS_END_MARKER;
 
 // Send byte to hardware port
 static inline void outb(uint16_t port, uint8_t val) {
-    __asm__ volatile ("outb %b0, %w1" : : "a"(val), "Nd"(port) : "memory");
+    __asm__ volatile ("outb %b0, %w1" 
+                        : 
+                        : "a"(val), "Nd"(port) 
+                        : "memory"
+                    );
 }
 
 void kernel_main(void) {
     
-    if(LIMINE_BASE_REVISION_SUPPORTED(limine_base_revision == false)) {
+    if(LIMINE_BASE_REVISION_SUPPORTED(limine_base_revision) == false) {
         hcf();
     }
 
     // Skipping framebuffer response check 
-     
-    
+
+    // Send data to serial port
+    outb(0x3f8, 'S'); 
     
     hcf();
 }
