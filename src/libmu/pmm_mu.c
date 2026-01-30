f0602ee69cec3078fef16a8deb5c9a26a9855cc4
lucasarabi <lucaarab03@gmail.com>
2026-01-30 16:41:00 -0500

    Implemented logic to fully initialize PMM bitmap

--- src/libmu/pmm_mu.c
+++ src/libmu/pmm_mu.c
@@ -4,6 +4,7 @@
 #define PRINTH write_serial_hex
 #define PRINTLN write_serial_str("\n");
 
+
 static uint64_t get_highest_usable_addr(struct limine_memmap_response* response) {
 
     if(response->entries == NULL) return 0;
@@ -11,7 +12,7 @@ static uint64_t get_highest_usable_addr(struct limine_memmap_response* response)
     const uint64_t num_entries = response->entry_count;
     struct limine_memmap_entry** entries = response->entries;
     uint64_t largest_addr = 0;    
-    
+
     PRINTS("Exploring RAM...");
     PRINTLN;
     PRINTS("Number of memory map entries: ");
@@ -26,20 +27,6 @@ static uint64_t get_highest_usable_addr(struct limine_memmap_response* response)
             continue;
         }
 
-        PRINTS("ptr: ");
-        PRINTH((uint64_t)current);
-        
-        PRINTS(" | Base: ");
-        PRINTH(current->base);        
-
-        PRINTS(" | Length: ");
-        PRINTH(current->length);
-
-        PRINTS(" | Type: ");
-        PRINTH(current->type); 
-
-        PRINTLN;
-
         uint64_t ceiling = current->base + current->length;
 
         if(current->type == LIMINE_MEMMAP_USABLE && ceiling > largest_addr) 
@@ -51,6 +38,30 @@ static uint64_t get_highest_usable_addr(struct limine_memmap_response* response)
     PRINTLN;
 
     return largest_addr;
+}
+
+// addr refers to PHYSICAL MEMORY address
+static inline uint64_t addr_to_index(uint64_t addr) {
+    return addr / 4096;
+}
+
+static inline uint64_t index_to_addr(uint64_t index) {
+    return index * 4096;
+}
+
+static inline void claim_frame(uint8_t* bitmap, uint64_t frame_index) {
+    uint64_t byte_index = frame_index / 8;
+    uint64_t bit_index = frame_index % 8; 
+
+    bitmap[byte_index] |= (1 << bit_index);
+
+}
+
+static inline void free_frame(uint8_t* bitmap, uint64_t frame_index) {
+    uint64_t byte_index = frame_index / 8;
+    uint64_t bit_index = frame_index % 8; 
+
+    bitmap[byte_index] &= ~(1 << bit_index);
 }
 
 static void pmm_init(struct limine_memmap_response* response) {
@@ -76,25 +87,42 @@ static void pmm_init(struct limine_memmap_response* response) {
     PRINTS("Bitmap start address: ");    
     PRINTH((uint64_t)bitmap_start);
     PRINTLN;
-    
+
+    PRINTS("Bitmap size: ");    
+    PRINTH(bitmap_size);
+    PRINTLN;
+
+
     memset(bitmap_start, 0xFF, bitmap_size);
     uint8_t* bitmap = bitmap_start; 
-    
-    // To-do -- Print entire memory map to understand
-    while(bitmap < bitmap_start + bitmap_size) {
-        PRINTH(*bitmap);
-        PRINTLN;
-        bitmap++;
+
+    // Free up USABLE memory
+    for(uint64_t i = 0; i < num_entries; i++) {
+        struct limine_memmap_entry* current = entries[i];
+
+        if(current->type == LIMINE_MEMMAP_USABLE) {
+
+            for(uint64_t addr = current->base; addr < (current->base + current->length); addr += 4096) {
+                uint64_t frame_index = addr_to_index(addr);
+                free_frame(bitmap, frame_index); 
+            }
+
+        }
     }
 
+    // Reclaim memory address storing pmm bitmap
+    for(uint64_t addr = (uint64_t)bitmap; addr < ((uint64_t)bitmap + bitmap_size); addr += 4096) {
+        uint64_t frame_index = addr_to_index(addr);
+        claim_frame(bitmap, frame_index);
+    }
+
+    PRINTS("Verifying Bitmap at 0x50000...");
+    PRINTLN;
+
+    for (uint64_t i = 0; i < 0x3f8 ; i++) {
+        PRINTS("Byte "); PRINTH(i); PRINTS(": ");
+        PRINTH(bitmap[i]); 
+        PRINTLN;
+    }
 }
-
-
-
-
-
-
-
-
-
 
