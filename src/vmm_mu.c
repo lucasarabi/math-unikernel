1f7c7885fb6cad008bc867e389b871b2fb5632bc
lucasarabi <lucaarab03@gmail.com>
2026-02-23 23:13:12 -0500

    Added usable ram metric to VMM init

--- src/vmm_mu.c
+++ src/vmm_mu.c
@@ -16,11 +16,6 @@
 #define PD_SHIFT   21
 #define PT_SHIFT   12
 
-#define KIB (1024ULL)  
-#define MIB (1024ULL * KIB)  
-#define GIB (1024ULL * MIB)  
-#define RAM_SIZE 512 * MIB 
-
 #define CR3_LOADED "VMM PML4 has been loaded to CR3 register.\n"
 
 struct vmm_context vmm; 
@@ -34,28 +29,24 @@ static void vmm_prep_pml4() {
 void vmm_map_virt_to_phys(uint64_t virt_addr, uint64_t phys_addr, uint64_t flags) {
     page_table_t* current_table = vmm.pml4_virt;
 
-    for(int level = 3; level >= 0; level--) {
+    for(int level = 3; level > 0; level--) {
         uint64_t index = (virt_addr >> (12 + (level*9))) & VMM_INDEX_MASK; 
-        pt_entry_t entry = current_table->entries[index];
 
-        if(level > 0) {
-            if(!(entry & VMM_PRESENT)) {
-                uint64_t new_table_phys = pmm_alloc();
+        if(!(current_table->entries[index] & VMM_PRESENT)) {
+            uint64_t new_table_phys = pmm_alloc();
+            page_table_t* new_table_virt = (page_table_t*)(new_table_phys + hhdm_offset);
+            memset(new_table_virt, 0, 4096);
 
-                page_table_t* new_table_virt = (page_table_t*)(new_table_phys + hhdm_offset);
-                memset(new_table_virt, 0 , 4096);
-            
-                current_table->entries[index] = new_table_phys | VMM_PRESENT | VMM_WRITEABLE;
-                
-                entry = current_table->entries[index];
-
-            }
-            current_table = (page_table_t*)(VMM_GET_ADDR(entry) + hhdm_offset);
-        } 
-        else {
-            current_table->entries[index] = (phys_addr & VMM_ADDR_MASK) | flags | VMM_PRESENT;
+            current_table->entries[index] = new_table_phys | VMM_PRESENT | VMM_WRITEABLE;
         }
+    
+        current_table = (page_table_t*)(VMM_GET_ADDR(current_table->entries[index]) + hhdm_offset);
     }
+
+    uint64_t pt_index = (virt_addr >> 12) & VMM_INDEX_MASK;
+    current_table->entries[pt_index] = (phys_addr & VMM_ADDR_MASK) | flags | VMM_PRESENT;
+
+    __asm__ volatile("invlpg (%0)" :: "r"(virt_addr) : "memory");
 }
 
 void vmm_map_range(uint64_t virt_start, uint64_t phys_start, uint64_t size, uint64_t flags) {
@@ -72,14 +63,22 @@ void vmm_init(struct limine_kernel_address_response* kernel_addr_response, struc
     uint64_t kernel_size = 0;
     for(uint64_t i = 0; i < memmap_response->entry_count; i++) {
         struct limine_memmap_entry* entry = memmap_response->entries[i];
-
         if(entry->type == LIMINE_MEMMAP_KERNEL_AND_MODULES) 
             kernel_size = entry->length;
     }
 
     vmm_map_range(kernel_addr_response->virtual_base, kernel_addr_response->physical_base, kernel_size, VMM_PRESENT | VMM_WRITEABLE);
 
-    vmm_map_range(hhdm_offset, 0x0, RAM_SIZE, VMM_PRESENT | VMM_WRITEABLE);
+    uint64_t usable_ram_bytes = 0;
+    for(uint64_t i = 0; i < memmap_response->entry_count; i++) {
+        struct limine_memmap_entry* entry = memmap_response->entries[i];
+        if(entry->type == LIMINE_MEMMAP_USABLE) {
+            vmm_map_range(entry->base + hhdm_offset, entry->base, entry->length, VMM_PRESENT | VMM_WRITEABLE);
+            usable_ram_bytes += entry->length;
+        }
+    }
+
+    PRINTF("Usable ram size (MB): ", usable_ram_bytes/(1024*1024)); PRINTLN;
 }
 
 void vmm_activate() {
