d6b251f374c187fb1633e134f7872b25983fe772
lucasarabi <lucaarab03@gmail.com>
2026-02-28 22:41:31 -0500

    minor changes

--- src/headers/io.h
+++ src/headers/io.h
@@ -3,7 +3,7 @@
 
 #include <stdint.h>
 
-void serial_init(void);
+void serial_init();
 void write_serial_str(const char* s);
 void write_serial_hex(uint64_t val);
 void write_serial_dec(uint64_t val);
