6dedb4f7ac5ad096b717b493396af04a632784ab
lucasarabi <lucaarab03@gmail.com>
2026-03-08 18:09:23 -0400

    Trivial edit

--- src/pci.c
+++ src/pci.c
@@ -14,8 +14,7 @@
 #define FOUND_KILLER_ID     "PCI: Found Killer NIC.\n"
 #define FOUND_REALTEK_ID     "PCI: Found Realtek NIC.\n"
 
-uint32_t pci_read_dword(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset)
-{
+uint32_t pci_read_dword(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset) {
     uint32_t lbus = (uint32_t)bus;
     uint32_t lslot = (uint32_t)slot;
     uint32_t lfunc = (uint32_t)func;
@@ -26,23 +25,18 @@ uint32_t pci_read_dword(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset)
     return inl(PCI_CONFIG_DATA);
 }
 
-uint16_t pci_scan_bus()
-{
-    for (uint16_t bus = 0; bus < 256; bus++)
-    {
-        for (uint8_t slot = 0; slot < 32; slot++)
-        {
+uint16_t pci_scan_bus() {
+    for (uint16_t bus = 0; bus < 256; bus++) {
+        for (uint8_t slot = 0; slot < 32; slot++) {
             uint32_t reg0 = pci_read_dword(bus, slot, 0, 0);
             uint16_t vendor = reg0 & 0xffff;
 
-            if (vendor != 0xffff)
-            {
+            if (vendor != 0xffff) {
                 uint32_t reg3 = pci_read_dword(bus, slot, 0, 0x08);
                 uint8_t base_class = (reg3 >> 24) & 0xff;
                 uint8_t sub_class = (reg3 >> 16) & 0xff;
 
-                if (base_class == 0x02 && sub_class == 0x00)
-                {
+                if (base_class == 0x02 && sub_class == 0x00) {
                     uint32_t bar0 = pci_read_dword(bus, slot, 0, 0x10);
 
                     switch(vendor) {
@@ -67,6 +61,5 @@ uint16_t pci_scan_bus()
             }
         }
     }
-
     return 0;
 }
